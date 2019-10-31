13b708b2db7caa514062c7d2988d104c5d07a4c1
Soivigol <soivigol1012@gmail.com>
2019-10-31 22:57:11 +0100

    Update dv-funciones.php

--- dv-funciones/dv-funciones.php
+++ dv-funciones/dv-funciones.php
@@ -18,7 +18,7 @@ include(DV_Ruta . 'funciones.php');
 // Register Script
 function custom_scripts() {
 
-	wp_register_script( 'dv-custom-js', DV_Ruta .'js/custom.js', array( 'jquery' ), '1.0', true );
+	wp_register_script( 'dv-custom-js', plugin_dir_url(__FILE__) .'js/custom.js', array( 'jquery' ), '1.0', true );
 	wp_enqueue_script( 'dv-custom-js' );
 	//Añadir solo en caso de usar Ajax para controlar la variable en el archivo js
 	//wp_localize_script('main','dv_var',['ajaxurl'=>admin_url('admin-ajax.php')]);
@@ -29,7 +29,7 @@ add_action( 'wp_enqueue_scripts', 'custom_scripts' );
 // Register Style
 function custom_styles() {
 
-	wp_register_style( 'dv-custom-style', DV_Ruta .'css/custom.css', false, '1.0' );
+	wp_register_style( 'dv-custom-style', plugin_dir_url(__FILE__) .'css/custom.css', false, '1.0' );
 	wp_enqueue_style( 'dv-custom-style' );
 
 }
